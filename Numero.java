ece5ae8e177e9e87988a7558019b88b19a2f81d7
herrera78 <dherrera@possibilitycompany.com>
2024-08-19 19:23:51 -0500

    Add files via upload
    Numbers class handles the number data type

---
+++ Numero.java
@@ -0,0 +1,17 @@
+class Numero{
+        private int n;
+        public int x, y;
+        
+        public Numero(){
+            n = 0;
+            x = 0;
+            y = 0;
+        }
+
+        public int getN(){
+            return n;
+        }
+        public void setN(int num){
+            n = num;
+        }
+}
